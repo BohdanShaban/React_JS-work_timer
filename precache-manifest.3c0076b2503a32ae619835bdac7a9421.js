37a21309472221d79b8188be951b98623dcee32e
Bohdan <bohdanshaban97@gmail.com>
2019-10-24 14:01:18 +0200

    Updates

---
+++ precache-manifest.3c0076b2503a32ae619835bdac7a9421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c06b54fd4a678f13f65cfe7a717df62",
+    "url": "/React_JS-work_timer/index.html"
+  },
+  {
+    "revision": "c5d7eb0f9266e36dfa77",
+    "url": "/React_JS-work_timer/static/css/main.9e9f8d20.chunk.css"
+  },
+  {
+    "revision": "64d9defc2a00d85bc16d",
+    "url": "/React_JS-work_timer/static/js/2.d6d57a46.chunk.js"
+  },
+  {
+    "revision": "c5d7eb0f9266e36dfa77",
+    "url": "/React_JS-work_timer/static/js/main.26141276.chunk.js"
+  },
+  {
+    "revision": "636837a1cd2d63f26761",
+    "url": "/React_JS-work_timer/static/js/runtime~main.673d0152.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React_JS-work_timer/static/media/logo.5d5d9eef.svg"
+  }
+]);
